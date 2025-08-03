542ea1c4509084f0f19ec35ac1a57db9a1e0687f
ekaterinavarzakova <ekaterinavarzakova@gmail.com>
2025-08-04 00:48:30 +0300

    Подтверждаю переименование uploads и другие правки

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/procure-pros/',  // ← ДОБАВЬ ЭТУ СТРОКУ
   server: {
     host: "::",
     port: 8080,
